98feba0f81125dcfb405b1caf8730ce7a81684af
Yogie-Anugrah <yogieanugrahramadhan@gmail.com>
2024-01-24 15:07:37 +0700

    add DB connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import pool from './db/db';
 
 const app = express();
 const port = 3000;
@@ -13,6 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
+});
+
+// add route to check db is alive
+app.get('/db', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.send(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
 });
 
 app.listen(port, () => {
