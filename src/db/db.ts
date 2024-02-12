712aeddf1055c5dc82f86194e13175543e8e984c
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2024-02-12 14:50:53 +0700

    feat: create api for product and products-list

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,12 +1,12 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 // TODO : Save the credential in Environment Variable
 const pool = new Pool({
-  user: "zfggiaiv",
-  password: "Z-vyv5W0F1jc7ESiUbFFirOtFWF9MdVL",
-  host: "rosie.db.elephantsql.com",
-  port: 5432, // default PostgreSQL port
-  database: "zfggiaiv",
+    user: "zfggiaiv",
+    password: "Z-vyv5W0F1jc7ESiUbFFirOtFWF9MdVL",
+    host: "rosie.db.elephantsql.com",
+    port: 5432, // default PostgreSQL port
+    database: "zfggiaiv",
 });
 
 export default pool;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import pool from './db/db';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import pool from "./db/db";
 
 const app = express();
 const port = 3000;
@@ -11,15 +11,38 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
 });
 
 // add route to check db is alive
-app.get('/db', async (req, res) => {
+app.get("/db", async (req, res) => {
     try {
-        const result = await pool.query('SELECT NOW()');
+        const result = await pool.query("SELECT NOW()");
         res.send(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+app.get("/products-list", async (req, res) => {
+    try {
+        const results = await pool.query("SELECT prod_id, prod_name, prod_exist, prod_main_img, prod_price, prod_flag FROM prod_tbl");
+
+        res.send(results.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+app.get("/product/:id", async (req, res) => {
+    try {
+        const { id } = req.params; 
+        const results = await pool.query("SELECT prod_id, prod_price, prod_name, prod_main_img, prod_desc FROM prod_tbl WHERE prod_id = $1", [id]);
+        const data = results.rows[0];
+        res.send(data);
     } catch (err) {
         console.error(err);
         res.status(500).send(err);
