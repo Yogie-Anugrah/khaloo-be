6a215749697730ab4a79ba48b51e81c964ea1289
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2024-02-16 17:07:56 +0700

    Product API (#2)
    * feat: create api for product and products-list
    
    * feat: create api to get product images and all products id
    
    * feat: create api to generate metadata product
    
    * refactor: split metadata api and static params
    
    ---------
    
    Co-authored-by: Yogie Anugrah <Yogieanugrahramadhan@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import pool from './db/db';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import pool from "./db/db";
 
 const app = express();
 const port = 3000;
@@ -11,15 +11,73 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
 });
 
 // add route to check db is alive
-app.get('/db', async (req, res) => {
+app.get("/db", async (req, res) => {
     try {
-        const result = await pool.query('SELECT NOW()');
+        const result = await pool.query("SELECT NOW()");
         res.send(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+// add route to get all products return id, name, image, price, exist, flag
+app.get("/product-list", async (req, res) => {
+    try {
+        const results = await pool.query("SELECT prod_id, prod_name, prod_exist, prod_main_img, prod_price, prod_flag FROM prod_tbl");
+        res.send(results.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+// add route to get product by id
+app.get("/product/:id", async (req, res) => {
+    try {
+        const { id } = req.params; 
+        const results = await pool.query("SELECT prod_id, prod_price, prod_name, prod_main_img, prod_desc FROM prod_tbl WHERE prod_id = $1", [id]);
+        res.send(results.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+// add route to get product images by id
+app.get("/product/:id/images", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const results = await pool.query("SELECT * FROM product_pict_id WHERE prod_id = $1", [id]);
+        res.send(results.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+// add route to get product's id to use on generating Static Params
+app.get("/products-id", async (req, res) => {
+    try {
+        const results = await pool.query("SELECT prod_id FROM prod_tbl");
+        res.send(results.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+// add route to get product's id to use on generating metadata
+app.get("/product-metadata/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const results = await pool.query("SELECT prod_name FROM prod_tbl WHERE prod_id = $1", [id]);
+        res.send(results.rows[0]);
     } catch (err) {
         console.error(err);
         res.status(500).send(err);
