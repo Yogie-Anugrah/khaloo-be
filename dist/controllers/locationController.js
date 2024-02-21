f8980595b5bbaf596cc9a5a6f642fd6c4e4e731b
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2024-02-21 18:13:55 +0700

    feat: update return object keys for findus page data (#16) (#17)

--- dist/controllers/locationController.js
+++ dist/controllers/locationController.js
@@ -17,8 +17,10 @@ const db_1 = __importDefault(require("../db/db"));
 // Fetch all locations without any projection
 const getAllLocations = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const results = yield db_1.default.query("SELECT * FROM location_tbl");
+        const client = yield db_1.default.connect();
+        const results = yield client.query("SELECT * FROM location_tbl");
         res.send(results.rows);
+        client.release();
     }
     catch (err) {
         next(err);
@@ -28,8 +30,35 @@ exports.getAllLocations = getAllLocations;
 // Get all locations with projection needed on find-us-page ordered by store_name and only if status = true (active)
 const getLocation = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const result = yield db_1.default.query("SELECT store_name, store_address, store_image, store_maps_url FROM location_tbl WHERE store_status=true ORDER BY store_name");
-        res.send(result.rows);
+        const client = yield db_1.default.connect();
+        // Validasi: Pastikan status toko adalah true
+        const query = `
+            SELECT 
+                store_name, 
+                store_address, 
+                store_image, 
+                store_maps_url 
+            FROM 
+                location_tbl 
+            WHERE 
+                store_status = true 
+            ORDER BY 
+                store_name
+        `;
+        const result = yield client.query(query);
+        if (result.rows.length === 0) {
+            res.send([]);
+        }
+        else {
+            const resultFormatting = result.rows.map((item) => ({
+                name: item.store_name,
+                address: item.store_address,
+                imageUrl: item.store_image,
+                mapsUrl: item.store_maps_url
+            }));
+            res.send(resultFormatting);
+        }
+        client.release();
     }
     catch (err) {
         next(err);
